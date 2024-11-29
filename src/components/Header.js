5fe2c6ed41b7e1802a07debe5916317e9d672788
Kim Seok Jin <kimseokjin6519@gmail.com>
2024-11-29 13:12:40 +0000

    User Management Page

---
+++ src/components/Header.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import defaultProfileImage from '../assets/images/default_profile.png';
+import defaultAppsImage from '../assets/images/grid.png';
+import defaultTasksImage from '../assets/images/hourglass.png';
+
+function Header () {
+   return (
+      <div>      
+         <div className="bg-blue-700 text-white p-4 flex justify-between items-center">
+            
+            {/* Left-aligned Flexbox (Menu, Google Admin, and Search) */}
+            <div className="flex items-center space-x-4">
+               <button className="text-white text-lg" style={{ fontFamily: 'Google Sans' }}>☰</button>
+               <h1 className="text-lg font-light pr-4 text-gray-100" style={{ fontFamily: 'Google Sans' }}>Google Admin</h1>
+               
+               <div className="flex items-center">
+                  <input
+                     type="text"
+                     placeholder="Search for users, groups or settings"
+                     className="text-xs p-2 rounded-l-lg text-gray-700 w-96 border-0 border-black"
+                     style={{ fontFamily: 'Google Sans' }}
+                  />
+                  <button className="text-xs bg-white text-blue-600 p-2 rounded-r-lg border-0 border-black border-l-0">
+                     🔍
+                  </button>
+               </div>
+            </div>
+            
+            {/* Right-aligned Flexbox (Default Tasks, Help Icon, Default Apps, Profile Picture) */}
+            <div className="flex items-center">
+               {/* Default Tasks */}
+               <button className="text-white">
+                  <img
+                     src={defaultTasksImage}
+                     alt="Default Tasks"
+                     className="w-4 h-4 mr-6 rounded-full"
+                  />
+               </button> 
+
+               {/* Help Icon */}
+               <button className="text-white">
+                  <div className="flex w-4 h-4 items-center justify-center bg-white mr-10 text-blue-700 rounded-full"
+                     style={{ fontFamily: 'Product Sans' }}>?</div>
+               </button> 
+
+               {/* Default Apps */}
+               <button className="text-white">
+                  <img
+                     src={defaultAppsImage}
+                     alt="Default Apps"
+                     className="w-4 h-4 bg-blue-700 mr-4"
+                  />
+               </button> 
+               
+               {/* Default Profile Picture */}
+               <button className="text-white">
+                  <img
+                     src={defaultProfileImage}
+                     alt="Profile"
+                     className="w-6 h-6 rounded-full"
+                  />
+               </button>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default Header;
--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -5,23 +5,23 @@ function UsersTable() {
   return (
     <div className="">
       <table className="min-w-full">
-        <thead className="h-6 w-full text-xs bg-gray-200" style={{fontFamily:'Google Sans'}}>
+        <thead className="h-8 w-full text-xs bg-gray-200 text-left" style={{fontFamily:'Google Sans'}}>
           <tr>
-            <th className="">Name</th>
-            <th className="">Email</th>
-            <th className="">Status</th>
-            <th className="">Last Sign In</th>
-            <th className="">Email Usage</th>
+            <th className="pl-2"><div className="flex"><div className="mr-4">☐</div><div className="mr-4">Name</div><div className="mr-2">↑</div></div></th>
+            <th className="text-gray-400">Email</th>
+            <th className="text-gray-400">Status</th>
+            <th className="text-gray-400">Last Sign In</th>
+            <th className="text-gray-400">Email Usage</th>
           </tr>
         </thead>
         <tbody>
           {Users.map((user, index) => (
-            <tr key={index} className="border-b">
-              <td className="px-4 py-2">{user.name}</td>
-              <td className="px-4 py-2">{user.email}</td>
-              <td className="px-4 py-2">{user.status}</td>
-              <td className="px-4 py-2">{user.last_sign_in}</td>
-              <td className="px-4 py-2">{user.email_usage}</td>
+            <tr key={index} className="border-b h-10 w-full text-xs text-left" style={{fontFamily:'Google Sans'}}>
+              <td className="py-2 pl-2"><div className="flex"><div className="mr-4">☐</div><div className="mr-4">{user.name}</div></div></td>
+              <td className="">{user.email}</td>
+              <td className="">{user.status}</td>
+              <td className="">{user.last_sign_in}</td>
+              <td className="">{user.email_usage}</td>
             </tr>
           ))}
         </tbody>
