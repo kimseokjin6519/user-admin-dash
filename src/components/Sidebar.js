d0f334a3898f379011381cfe37ce924b8f82ad35
Kim Seok Jin <kimseokjin6519@gmail.com>
2024-12-04 23:10:31 +0900

    Sidebar Updates

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,7 @@ function SideBar({ headerHeight }) {
    const [allItemsVisible, setAllItemsVisible] = useState(false);
 
    useEffect(() => {
-      const delays = [0, 500, 1000, 1500, 2000, 2500];
+      const delays = [0, 250, 500, 750, 1000, 1250];
       delays.forEach((delay, index) => {
          setTimeout(() => {
             setVisibleItems((prev) => [...prev, index]);
@@ -14,20 +14,43 @@ function SideBar({ headerHeight }) {
             }
          }, delay);
       });
-      return () => clearTimeout();
+
+      const style = document.createElement('style');
+      style.innerHTML = `
+         @keyframes fadeInOut {
+            0% {
+               opacity: 0;
+            }
+            50% {
+               opacity: 1;
+            }
+            100% {
+               opacity: 0;
+            }
+         }
+         .fadeInOut {
+            animation: fadeInOut 1s ease-out forwards;
+         }
+      `;
+      document.head.appendChild(style);
+      return () => {
+         document.head.removeChild(style);
+      };
    }, []);
 
    const items = ['Dashboard', 'Users', 'Groups', 'Settings', 'Reports'];
+
    return (
       <div className="fixed w-[250px] top-0 left-0 bg-white">
          <div className="flex w-[250px] bg-blue-800 mb-4">
             <div
-               className={`absolute top-0 left-0 w-0 h-0 border-l-[15px] border-b-[15px] border-l-transparent border-b-white transition-opacity duration-500 ease-out ${ allItemsVisible ? 'opacity-0' : 'opacity-100'}`}></div>
+               className={`absolute top-0 left-0 w-0 h-0 border-l-[15px] border-b-[15px] border-l-transparent border-b-white fadeInOut`}>
+            </div>
             <div className="flex ml-8 text-sm text-white bg-blue-800 items-center" style={{ height: `${headerHeight}px`, fontFamily: 'Google Sans' }}>Home</div>
             <div className="flex ml-auto mr-4 flex text-xs text-white bg-blue-800 items-center">▼</div>
          </div>
          {items.map((item, index) => (
-            <div key={index} className={`ml-14 mb-2 text-sm transition-transform duration-500 ${visibleItems.includes(index) ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-5'}`} style={{ fontFamily: 'Google Sans' }}>
+            <div key={index} className={`ml-14 mb-2 text-sm transition-transform duration-750 ${visibleItems.includes(index) ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-5'}`} style={{ fontFamily: 'Google Sans' }}>
                {item}
             </div>
          ))}
